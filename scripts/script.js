1f568aa0a3cbb0da9dd864f45d2ee23425c09260
Vladislav Polonik <v_polonik@inbox.ru>
2021-02-01 22:34:39 +0300

    First commit

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+// ___ Task 1 ___
+console.log("___ Task 1 ___");
+let i = 1;
+while (i <=50) {
+    console.log(i);
+    i++
+}
+i = 35
+console.log("___ ___ ___");
+while (i >= 8){
+    console.log(i);
+    i--
+}
+
+// ___ Task 2 ___
+console.log("___ Task 2 ___");
+i = 89;
+let text_task2 = "";
+while (i <= 11){
+    text_task2 += (i + "<br/")
+    i--;
+}
